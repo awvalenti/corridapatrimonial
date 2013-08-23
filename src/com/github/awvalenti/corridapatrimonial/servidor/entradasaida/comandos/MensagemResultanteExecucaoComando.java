9c2f05f9eb6f9c4a7f592a0200b99c6c55dd95ad
Andre Willik Valenti <awvalenti@gmail.com>
2013-08-23 15:18:23 -0300

    Comunicação cliente-servidor, troca de chaves etc.

---
+++ src/com/github/awvalenti/corridapatrimonial/servidor/entradasaida/comandos/MensagemResultanteExecucaoComando.java
@@ -0,0 +1,10 @@
+package com.github.awvalenti.corridapatrimonial.servidor.entradasaida.comandos;
+
+public enum MensagemResultanteExecucaoComando {
+	NENHUMA_MENSAGEM,
+
+	JOGADOR_ENTROU,
+
+	COMANDO_NAO_RECONHECIDO,
+
+}
