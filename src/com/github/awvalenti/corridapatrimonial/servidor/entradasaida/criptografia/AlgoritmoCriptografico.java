9010e5d83572fe67d90dc4d1f78b692c1ba5f0d6
Andre Willik Valenti <awvalenti@gmail.com>
2013-08-23 11:46:38 -0300

    Cifra de César estendida

--- src/com/github/awvalenti/corridapatrimonial/servidor/entradasaida/criptografia/AlgoritmoCriptografico.java
+++ src/com/github/awvalenti/corridapatrimonial/servidor/entradasaida/criptografia/AlgoritmoCriptografico.java
@@ -3,38 +3,25 @@ package com.github.awvalenti.corridapatrimonial.servidor.entradasaida.criptograf
 public enum AlgoritmoCriptografico {
 	SEM_CRIPTOGRAFIA {
 		@Override
-		public String decifrar(String textoClaro, int chave) {
+		public String cifrar(String textoClaro, int chave) {
 			return textoClaro;
 		}
 
 		@Override
-		public String cifrar(String textoClaro, int chave) {
-			// TODO Auto-generated method stub
-			return null;
+		public String decifrar(String textoCifrado, int chave) {
+			return textoCifrado;
 		}
 	},
 
-	CIFRA_DE_CESAR {
+	CIFRA_DE_CESAR_ESTENDIDA {
+		private static final int MODULO_ARITMETICA_CHAR = Character.MAX_VALUE + 1;
+
 		@Override
 		public String cifrar(String textoClaro, int chave) {
 			StringBuilder textoCifrado = new StringBuilder();
 
 			for (int i = 0; i < textoClaro.length(); ++i) {
-				char caractereCifrado = textoClaro.charAt(i);
-
-				char caractereFinal;
-
-				if (!Character.isLetter(caractereCifrado)) {
-					caractereFinal = caractereCifrado;
-
-				} else {
-					char extremo = Character.isUpperCase(caractereCifrado) ? 'Z' : 'z';
-					int caractereDecifrado = caractereCifrado + chave;
-					if (caractereDecifrado > extremo) caractereDecifrado -= 26;
-					caractereFinal = (char) caractereDecifrado;
-				}
-
-				textoCifrado.append(caractereFinal);
+				textoCifrado.append((char) ((textoClaro.charAt(i) + chave) % MODULO_ARITMETICA_CHAR));
 			}
 
 			return textoCifrado.toString();
@@ -45,37 +32,17 @@ public enum AlgoritmoCriptografico {
 			StringBuilder textoDecifrado = new StringBuilder();
 
 			for (int i = 0; i < textoCifrado.length(); ++i) {
-				char caractereCifrado = textoCifrado.charAt(i);
-
-				char caractereFinal;
-
-				if (!Character.isLetter(caractereCifrado)) {
-					caractereFinal = caractereCifrado;
-
-				} else {
-					char extremo = Character.isUpperCase(caractereCifrado) ? 'A' : 'a';
-					int caractereDecifrado = caractereCifrado - chave;
-					if (caractereDecifrado < extremo) caractereDecifrado += 26;
-					caractereFinal = (char) caractereDecifrado;
-				}
-
-				textoDecifrado.append(caractereFinal);
+				textoDecifrado.append((char) ((textoCifrado.charAt(i) - chave + MODULO_ARITMETICA_CHAR) % MODULO_ARITMETICA_CHAR));
 			}
 
 			return textoDecifrado.toString();
 		}
 	},
+
 	;
 
 	public abstract String cifrar(String textoClaro, int chave);
 	public abstract String decifrar(String textoCifrado, int chave);
-
-	public static void main(String[] args) {
-		String cifrado = CIFRA_DE_CESAR.cifrar("aAzZ comprar 123 $%%", 2);
-		System.out.println(cifrado);
-		System.out.println(CIFRA_DE_CESAR.decifrar(cifrado, 2));
-		System.out.println(-1 % 26);
-	}
 
 }
 
